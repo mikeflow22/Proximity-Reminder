5d4a9363f38240c7c39b8ecb41562d862060fd5c
Michael R.F <mrflowers22@uwalumni.com>
2020-01-30 08:45:41 -0800

    added folders in the project navigator. assigned scenes to applicable source code files

---
+++ ReminderApp/View Controllers/DetailReminderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailReminderViewController.swift
+//  ReminderApp
+//
+//  Created by Michael Flowers on 1/30/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class DetailReminderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
