5c17443a4c551fdb5aa3b531df92de29532d0535
Michael R.F <mrflowers22@uwalumni.com>
2020-01-30 16:35:08 -0800

    implemented function to request for notifications

--- ReminderApp/View Controllers/DetailReminderViewController.swift
+++ ReminderApp/View Controllers/DetailReminderViewController.swift
@@ -7,23 +7,111 @@
 //
 
 import UIKit
+import MapKit
+import UserNotifications
 
 class DetailReminderViewController: UIViewController {
     
+    //MARK: - INSTANCE PROPERTIES
     var addressString: String? {
         didSet {
             print("addressString was hit")
             print("this is what was passed in: \(String(describing: addressString))")
+            if let string = addressString {
+                getAddressFrom(searchResultsTitle: string)
+            }
+        }
+    }
+    var coordinate: CLLocationCoordinate2D? {
+        didSet {
+            print("coordinate was set: \(coordinate.debugDescription)")
         }
     }
     
+    //MARK: - IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var reminderTextField: UITextField!
+    @IBOutlet weak var enterGeofenceProperties: UIButton!
+    @IBOutlet weak var exitGeofenceProperties: UIButton!
+    @IBOutlet weak var radiusTextField: UITextField!
+    @IBOutlet weak var setReminderProperties: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
 
-        // Do any additional setup after loading the view.
     }
     
+    //MARK: - IBActions
+    @IBAction func EnterGeofenceButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func exitGeofenceButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func setReminderButtonTapped(_ sender: UIButton) {
+        requestAuthorizationForNotifications()
+    }
+    
+    
+    //MARK: - Instance Methods
+    
+    func getAddressFrom(searchResultsTitle: String){
+              let request = MKLocalSearch.Request()
+              request.naturalLanguageQuery = searchResultsTitle
+              let localSearch = MKLocalSearch(request: request)
+              
+              localSearch.start { [unowned self] (response, error) in
+                  if let error = error {
+                      print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                      return
+                  }
+                  if response == nil {
+                      print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                      return
+                  }
+                  
+                  //get the lat and long associated with the string/word/name we just go back from this call
+                  guard let lat = response?.boundingRegion.center.latitude, let long = response?.boundingRegion.center.longitude else {
+                      print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                      return
+                  }
+                  
+                  //create annotation to add to map
+                  let annotation = MKPointAnnotation()
+                  annotation.title = searchResultsTitle
+                let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+                self.coordinate = coordinate
+                  annotation.coordinate = coordinate
+                  
+                  //add it to mapview
+                  self.mapView.addAnnotation(annotation)
+                self.setSpan(with: coordinate)
+              }
+      }
+    
+    func setSpan(with coordinate: CLLocationCoordinate2D){
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        self.mapView.setRegion(region, animated: true)
+    }
 
+    func requestAuthorizationForNotifications(){
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            if let error = error {
+                print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                return
+            }
+            if success {
+                    print("Success: \(success.description) in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+              
+            } else {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                return
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
@@ -34,4 +122,11 @@ class DetailReminderViewController: UIViewController {
     }
     */
 
+}
+extension DetailReminderViewController: MKMapViewDelegate {
+    
+}
+
+extension DetailReminderViewController: UNUserNotificationCenterDelegate {
+    
 }
