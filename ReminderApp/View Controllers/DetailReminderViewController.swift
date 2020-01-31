2bbdcb1efd104d674a06f8c50633530c2960c2bf
Michael R.F <mrflowers22@uwalumni.com>
2020-01-30 17:00:11 -0800

    created function to setup local notification

--- ReminderApp/View Controllers/DetailReminderViewController.swift
+++ ReminderApp/View Controllers/DetailReminderViewController.swift
@@ -40,7 +40,7 @@ class DetailReminderViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-
+        
     }
     
     //MARK: - IBActions
@@ -51,6 +51,10 @@ class DetailReminderViewController: UIViewController {
     }
     
     @IBAction func setReminderButtonTapped(_ sender: UIButton) {
+      
+        
+        
+//        guard let note = reminderTextField.text, !note.isEmpty, let shouldTriggerOnEntry =
         requestAuthorizationForNotifications()
     }
     
@@ -58,45 +62,45 @@ class DetailReminderViewController: UIViewController {
     //MARK: - Instance Methods
     
     func getAddressFrom(searchResultsTitle: String){
-              let request = MKLocalSearch.Request()
-              request.naturalLanguageQuery = searchResultsTitle
-              let localSearch = MKLocalSearch(request: request)
-              
-              localSearch.start { [unowned self] (response, error) in
-                  if let error = error {
-                      print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
-                      return
-                  }
-                  if response == nil {
-                      print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
-                      return
-                  }
-                  
-                  //get the lat and long associated with the string/word/name we just go back from this call
-                  guard let lat = response?.boundingRegion.center.latitude, let long = response?.boundingRegion.center.longitude else {
-                      print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
-                      return
-                  }
-                  
-                  //create annotation to add to map
-                  let annotation = MKPointAnnotation()
-                  annotation.title = searchResultsTitle
-                let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
-                self.coordinate = coordinate
-                  annotation.coordinate = coordinate
-                  
-                  //add it to mapview
-                  self.mapView.addAnnotation(annotation)
-                self.setSpan(with: coordinate)
-              }
-      }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchResultsTitle
+        let localSearch = MKLocalSearch(request: request)
+        
+        localSearch.start { [unowned self] (response, error) in
+            if let error = error {
+                print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                return
+            }
+            if response == nil {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                return
+            }
+            
+            //get the lat and long associated with the string/word/name we just go back from this call
+            guard let lat = response?.boundingRegion.center.latitude, let long = response?.boundingRegion.center.longitude else {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                return
+            }
+            
+            //create annotation to add to map
+            let annotation = MKPointAnnotation()
+            annotation.title = searchResultsTitle
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            self.coordinate = coordinate
+            annotation.coordinate = coordinate
+            
+            //add it to mapview
+            self.mapView.addAnnotation(annotation)
+            self.setSpan(with: coordinate)
+        }
+    }
     
     func setSpan(with coordinate: CLLocationCoordinate2D){
         let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         self.mapView.setRegion(region, animated: true)
     }
-
+    
     func requestAuthorizationForNotifications(){
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
             if let error = error {
@@ -104,24 +108,44 @@ class DetailReminderViewController: UIViewController {
                 return
             }
             if success {
-                    print("Success: \(success.description) in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
-              
+                print("Success: \(success.description) in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                
             } else {
                 print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
                 return
             }
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func createLocalNotification(address: String, body: String, coordinate: CLLocationCoordinate2D, enterRegion: Bool, exitRegion: Bool, radius: Double){
+        let notificationContent = UNMutableNotificationContent()
+        //dress up notification
+        notificationContent.title = "Reminder for location: \(address)"
+        notificationContent.body = body
+        notificationContent.sound = UNNotificationSound.default
+        
+        //CREATE the circular region to monitor and set if it should be triggered on entry  or exit or both
+        let circularRegion = CLCircularRegion(center: coordinate, radius: radius, identifier: address)
+        circularRegion.notifyOnEntry = enterRegion
+        circularRegion.notifyOnExit = exitRegion
+        
+        //create the trigger
+        let locationTrigger = UNLocationNotificationTrigger(region: circularRegion, repeats: true)
+        
+        //every local notification trigger needs a request
+        let request = UNNotificationRequest(identifier: "remote trigger for: \(address)", content: notificationContent, trigger: locationTrigger)
+        
+        //add the request to the UNNotificationCenter singleton
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                return
+            } else {
+                print("WE SUCCEEDED SENDING THE LOCAITON TRIGGER")
+            }
+        }
     }
-    */
-
+    
 }
 extension DetailReminderViewController: MKMapViewDelegate {
     
